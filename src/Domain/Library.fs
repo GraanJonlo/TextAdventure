c40040762335a360e86690bc19dd42611d991670
Andy Grant <andy.a.grant@gmail.com>
2017-12-05 22:07:26 +0000

    Adjective type

--- src/Domain/Library.fs
+++ src/Domain/Library.fs
@@ -1,5 +1,14 @@
 namespace Domain
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module Words =
+    type Adjective =
+    | Opinion of string // unusual, lovely, beautiful
+    | Size of string // big, small, tall
+    | PhysicalQuality of string // thin, rough, untidy
+    | Shape of string // round, square, rectangular
+    | Age of string // young, old, youthful
+    | Colour of string // blue, red, pink
+    | Origin of string // Dutch, Japanese, Turkish
+    | Material of string // metal, wood, plastic
+    | Type of string // general-purpose, four-sided, U-shaped
+    | Purpose of string // cleaning, hammering, cooking
