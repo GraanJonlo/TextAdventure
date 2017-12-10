f7e2c258c36b06e8e2a1b1f9b43218cd73eb75b0
Andy Grant <andy.a.grant@gmail.com>
2017-12-10 10:08:00 +0000

    To string determiners, prepositions, adjectives, nouns and noun phrases.

--- src/Domain/Library.fs
+++ src/Domain/Library.fs
@@ -2,10 +2,20 @@ namespace Domain
 
 module Words =
     type Determiner =
+    | A
     | The
+    
+    let detToString followedByAVowel =
+        function
+        | A -> if followedByAVowel then "an" else "a"
+        | The -> "the"
 
     type Preposition =
     | Up
+
+    let pToString =
+        function
+        | Up -> "up"
 
     type Adjective =
     | Opinion of string // unusual, lovely, beautiful
@@ -19,6 +29,41 @@ module Words =
     | Type of string // general-purpose, four-sided, U-shaped
     | Purpose of string // cleaning, hammering, cooking
 
+    type AdjectiveSortOrder = Adjective -> int
+
+    let adjSortOrder =
+        function
+        | Opinion _ -> 1
+        | Size _ -> 2
+        | PhysicalQuality _ -> 3
+        | Shape _ -> 4
+        | Age _ -> 5
+        | Colour _ -> 6
+        | Origin _ -> 7
+        | Material _ -> 8
+        | Type _ -> 9
+        | Purpose _ -> 10
+    
+    // Allows List.sortBy adjSortOrder adjectives
+
+    let adjToString =
+        function
+        | Opinion x -> x
+        | Size x -> x
+        | PhysicalQuality x -> x
+        | Shape x -> x
+        | Age x -> x
+        | Colour x -> x
+        | Origin x -> x
+        | Material x -> x
+        | Type x -> x
+        | Purpose x -> x
+    
+    let adjsToString =
+        List.sortBy adjSortOrder
+        >> Seq.map adjToString
+        >> String.concat ", "
+
     type Noun = Noun of string
 
     type Verb = Verb of string
@@ -28,6 +73,25 @@ module Words =
         determiner:Determiner option
         adjectives:Adjective list
     }
+    
+    let startsWithVowel x =
+        x <> "" && List.contains x.[0] ['a';'e';'i';'o';'u']
+
+    let npToString x =
+        let adjStr = adjsToString x.adjectives
+        
+        let detStr =
+            match x.determiner with
+            | Some y -> detToString (startsWithVowel adjStr) y
+            | None -> ""
+
+        let nounStr =
+            match x.head with
+            | Noun n -> n
+
+        [detStr; adjStr; nounStr]
+        |> Seq.filter (fun x -> x<>"")
+        |> String.concat " "
 
     type PrepositionalPhrase = {
         head:Preposition
@@ -58,6 +122,26 @@ module Words =
     }
 
     let getTheRedKey = {
-        head = Verb "pick"
+        head = Verb "get"
         complement = Object theRedKey
     }
+
+    type Item = {
+        noun:Noun
+        adjectives:Adjective list
+        shortDescription:string
+        longDescription:string
+    }
+
+    let npOfItem x = {
+        head = x.noun
+        determiner = Some A
+        adjectives = x.adjectives
+    }
+
+    type ItemMatchResult =
+    | Exact of Item
+    | Ambiguous of Item list
+    | None
+
+    type MatchItem = Item list -> NounPhrase -> ItemMatchResult
